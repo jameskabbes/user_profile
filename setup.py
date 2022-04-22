5377d84a7b446fa439b354fd251a710d5dfceca0
James Kabbes <james.kabbes@gmail.com>
2022-04-22 16:35:28 -0500

    removing attempts to exclude package data from build

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-    setup(
-
-        exclude_package_data = {'user_profile':
-        [
-            'Users/james.py'
-        ]
-        }
-
-    )
+    setup()
